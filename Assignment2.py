dae91f8c7b982fe50f80266861b5bb192ac72fa1
Yuricin <ameena2232005@gmail.com>
2025-10-08 22:31:26 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,23 +1,19 @@
 def main():
     num=int(input("Enter an integer:"))
     print(odd_even(num),"\n\n")
-    
-def main2():
     print(sum())
-
+    
 def odd_even(num):
     if num%2==0:
-        return f"The {num} is an even number"
+        return f"The number {num} is an even number"
     else:
-         return f"The {num} is an odd number"
+         return f"The number {num} is an odd number"
          
 def sum():
          i=0
          sum=0
-         while i<=50:
+         for i in range(0,51):
              sum+=i
-         return sum
+         return f"The sum of numbers from 1 to 50 is {sum}"
              
-if  __name__== "__ main__":
-    main()  
-    main2()
+main()  
